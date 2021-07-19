7a8927983e79cda9dad93f47918f44f7208325b9
DariaKat <dari.aria@yandex.ru>
2021-07-19 17:10:21 +0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -16,14 +16,14 @@ function imageComparison(selector) {
 
     comparison
         .find('.image-comparison__slider')
-        .on('dragstart', () => false) // отмена станд. drug&drop 
+        .on('dragstart', () => false) 
         .on('mousedown', function(e) {
             let slider = $(this);
             let doc = $(document).on('mousemove', (e) => {
                 let offset = e.pageX - comparison.position().left;
                 let width = comparison.width();
 
-                // установим границы, чтобы ползунок не выходил 
+              
                 if (offset < 0) offset = 0;
                 if (offset > width) offset = width;
 
